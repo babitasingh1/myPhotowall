8b20a64402d53acbf543f3340cac3bd1fbf96a6c
Babita Singh <babita.singh1@gmail.com>
2021-03-12 14:24:35 +0100

    Added routes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,66 +1,74 @@
-import React, { useState } from 'react';
-import PhotoWall from './PhotoWall';
-import AddPhoto from './AddPhoto';
-import { Route } from 'react-router-dom';
+import React, {Component} from 'react'
+ import Employee from './Employee'
+import AddEmployee from './AddEmployee'
+import {Route} from 'react-router-dom'
+  
 
-function Main() {
-  const [posts, setposts] = useState([
-    {
-      id: 0,
-      description: 'beautiful flower',
-      imageLink:
-        'https://i.pinimg.com/originals/ca/97/91/ca9791ad522c7f2b6e77c2cbd1e80665.jpg',
-    },
-    {
-      id: 1,
-      description: 'beautiful landscape',
-      imageLink:
-        'https://images.unsplash.com/photo-1560421683-2587f1591d30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1191&q=80',
-    },
-    {
-      id: 2,
-      description: 'On a vacation!',
-      imageLink:
-        'https://images.unsplash.com/photo-1598899018780-e9d6167f8da3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    },
-  ]);
+class Main extends Component{
+    constructor() {
+        super()
+        this.state = {
+            Person: [{name:'Babita',
+                Empid:158476,
+                City:'Utrecht'},
 
-  function removePhoto(postRemoved) {
-    const newposts = posts.filter((post) => post.id !== postRemoved);
-    setposts(newposts);
-  }
+                {name:'Radha',
+                 Empid:23789,
+                 City:'Banglore'},
 
-  function addPhoto(postSubmitted) {
-    const newposts = posts.concat([postSubmitted]);
-    setposts(newposts);
-  }
+                 {name:'Aarti',
+                  Empid:75463,
+                  City:'Pune'}
+],
+screen: 'Employee' //Employee,addEmployee
+        }
 
-  return (
-    <div>
-      <Route
-        exact
-        path='/'
-        render={() => (
-          <div>
-            <h1>PhotoWall</h1>
-            <PhotoWall posts={posts} onRemovePhoto={removePhoto} />
-          </div>
-        )}
-      />
+        this.removeEmployee = this.removeEmployee.bind(this);
+        
+        
+        this.Navigate = this.Navigate.bind(this);
+    }
+    
+    removeEmployee(personRemoved){
+        console.log(personRemoved.City)
+        this.setState((state) => ({
+            Person: state.Person.filter(Person => Person !== personRemoved )
+        }))
 
-      <Route
-        path='/AddPhoto'
-        render={({ history }) => (
-          <AddPhoto
-            onAddPhoto={(addedPost) => {
-              addPhoto(addedPost);
-              history.push('/');
-            }}
-          />
-        )}
-      />
-    </div>
-  );
+    }  
+
+    addEmployee(postSubmitted){
+        this.setState(state =>({
+            Person: state.Person.concat([postSubmitted]) 
+            
+        }))
+    }
+
+    Navigate(){
+        this.setState({
+            screen: 'AddEmployee'
+        })
+    }
+
+    render(){
+        return <div>
+              <Route exact path="/" render={() => (
+        <div>
+            <h1>Employee List </h1>
+            <Employee persons = {this.state.Person} onremoveEmployee={this.removeEmployee} onNavigate = {this.Navigate} />
+       </div> )}/>
+    
+
+             <Route path = "/AddEmployee" render ={({history}) => (<AddEmployee onaddEmployee={(addedEmployee)=>{
+                 this.addEmployee(addedEmployee)
+                 console.log(addedEmployee);
+                 history.push('/')
+
+             }} /> )}/>
+            
+
+            </div>
+    }
 }
 
-export default Main;
+ export default Main;
